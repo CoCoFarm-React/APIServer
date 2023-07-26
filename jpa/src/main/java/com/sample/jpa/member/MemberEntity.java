5d3db525e197212f0589c3c7fadc769efc1836c2
whtkdgml3627 <46257969+joungjaehyun@users.noreply.github.com>
2023-07-26 14:57:55 +0900

    testbranch

---
+++ jpa/src/main/java/com/sample/jpa/member/MemberEntity.java
@@ -0,0 +1,14 @@
+package com.sample.jpa.member;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class MemberEntity {
+
+    
+    @Id
+    private String 
+
+}
