c2e9d07a88971477777b3c9370fad2efcbfa3d1d
joungjaehyun <joungjaehyun1008@gmail.com>
2023-07-31 12:05:29 +0900

    category

--- apiserver/src/test/java/com/project/apiserver/repository/MemberRepositoryTests.java
+++ apiserver/src/test/java/com/project/apiserver/repository/MemberRepositoryTests.java
@@ -20,43 +20,43 @@ public class MemberRepositoryTests {
     @Autowired
     private MemberRepository repository;
 
-    @Test
-    public void insertTest(){
-
-        for(int i = 0; i<100; i++){
-        Member member = Member.builder()
-        .email("aaa" +i+"@email.com")
-        .pw("1111")
-        .nickname("nickname"+ i)
-        .build();
-
-        switch(i%2){
-
-            case 0:
-            member.addRole(MemberRole.FARMER);
-            break;
-            case 1:
-            member.addRole(MemberRole.CONSUMER);
-            break;
-        }
-        
-        repository.save(member);
-        }
-    }
-
-
     // @Test
-    // @Transactional
-    // public void readTest(){
+    // public void insertTest(){
 
-    //     // List<Member> list = repository.findAll();
-    //     // log.info(list);
-    //     // log.info("----------");
-    //     // list.forEach(mb ->log.info(mb.getRole()));
+    //     for(int i = 0; i<100; i++){
+    //     Member member = Member.builder()
+    //     .email("aaa" +i+"@email.com")
+    //     .pw("1111")
+    //     .nickname("nickname"+ i)
+    //     .build();
+
+    //     switch(i%2){
+
+    //         case 0:
+    //         member.addRole(MemberRole.FARMER);
+    //         break;
+    //         case 1:
+    //         member.addRole(MemberRole.CONSUMER);
+    //         break;
+    //     }
         
-    //     List<Member> list = repository.findByRole(MemberRole.CONSUMER);
-
-    //     log.info(list);
-    //     list.forEach(member->log.info(member.getRole()));
+    //     repository.save(member);
+    //     }
     // }
+
+
+    @Test
+    @Transactional
+    public void readTest(){
+
+        // List<Member> list = repository.findAll();
+        // log.info(list);
+        // log.info("----------");
+        // list.forEach(mb ->log.info(mb.getRole()));
+        
+        List<Member> list = repository.findByRole(MemberRole.CONSUMER);
+
+        log.info(list);
+        list.forEach(member->log.info(member.getRole()));
+    }
 }
