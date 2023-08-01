c1defcee5ccbb0dfb57354816874d93ebe328a71
joungjaehyun <joungjaehyun1008@gmail.com>
2023-08-01 12:54:33 +0900

    repli

--- apiserver/src/test/java/com/project/apiserver/service/ReplyServiceTests.java
+++ apiserver/src/test/java/com/project/apiserver/service/ReplyServiceTests.java
@@ -26,22 +26,3 @@ public class ReplyServiceTests {
     }
 
 }
-package com.project.apiserver.service;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.project.apiserver.reply.dto.ReplyDTO;
-import com.project.apiserver.reply.service.ReplyService;
-
-import lombok.extern.log4j.Log4j2;
-
-@SpringBootTest
-@Log4j2
-public class ReplyServiceTests {
-    
-
-
-    
-}
