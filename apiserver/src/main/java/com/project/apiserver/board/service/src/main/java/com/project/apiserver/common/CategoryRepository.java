ebaeed80c78f9f6685a9c800e5c8cae19f3ba90e
joungjaehyun <joungjaehyun1008@gmail.com>
2023-08-01 11:39:50 +0900

    reply

--- apiserver/src/main/java/com/project/apiserver/board/service/src/main/java/com/project/apiserver/common/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.apiserver.common;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository  extends JpaRepository<Category,Integer>{
-    
-}
