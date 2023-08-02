56ed950fb1dada711163e9c3882d3ed3afe9d0a9
chanunu90 <128343652+chanunu90@users.noreply.github.com>
2023-08-02 14:15:28 +0900

    APIserver final

--- apiserver/src/test/java/com/project/apiserver/repository/BoardRepositoryTests.java
+++ apiserver/src/test/java/com/project/apiserver/repository/BoardRepositoryTests.java
@@ -1,100 +1,100 @@
-package com.project.apiserver.repository;
+// package com.project.apiserver.repository;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+// import java.util.Arrays;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.annotation.Commit;
 
-import com.project.apiserver.board.dto.BoardListDTO;
-import com.project.apiserver.board.dto.IBoardListDTO;
-import com.project.apiserver.board.entity.Board;
-import com.project.apiserver.board.repository.BoardRepository;
-import com.project.apiserver.common.Category;
-import com.project.apiserver.common.PageRequestDTO;
-import com.project.apiserver.common.PageResponseDTO;
-import com.project.apiserver.member.entity.Member;
+// import com.project.apiserver.board.dto.BoardListDTO;
+// import com.project.apiserver.board.dto.IBoardListDTO;
+// import com.project.apiserver.board.entity.Board;
+// import com.project.apiserver.board.repository.BoardRepository;
+// import com.project.apiserver.common.Category;
+// import com.project.apiserver.common.PageRequestDTO;
+// import com.project.apiserver.common.PageResponseDTO;
+// import com.project.apiserver.member.entity.Member;
 
-import jakarta.transaction.Transactional;
-import lombok.extern.log4j.Log4j2;
+// import jakarta.transaction.Transactional;
+// import lombok.extern.log4j.Log4j2;
 
-@SpringBootTest
-@Log4j2
-public class BoardRepositoryTests {
+// @SpringBootTest
+// @Log4j2
+// public class BoardRepositoryTests {
 
-    @Autowired
-    private BoardRepository boardRepository;
+//     @Autowired
+//     private BoardRepository boardRepository;
 
-    @Test
-    public void addBoard() {
+//     @Test
+//     public void addBoard() {
 
-        Category category = Category
-        .builder()
-        .cateno(1)
-        .catename("관리자문의")
-        .build();
+//         Category category = Category
+//         .builder()
+//         .cateno(1)
+//         .catename("관리자문의")
+//         .build();
 
-        Member member = Member.builder().email("aaa0@email.com").build();
+//         Member member = Member.builder().email("aaa0@email.com").build();
         
-        log.info("Start insert");
+//         log.info("Start insert");
 
-        for (int i = 0; i < 100; i++) {
+//         for (int i = 0; i < 100; i++) {
 
-            Board board = Board.builder()
-                    .category(category)
-                    .member(member)
-                    .title("게시판 제목" + i)
-                    .content("내용드릉ㄴ믕ㄴㅁ" + i)
-                    .build();
+//             Board board = Board.builder()
+//                     .category(category)
+//                     .member(member)
+//                     .title("게시판 제목" + i)
+//                     .content("내용드릉ㄴ믕ㄴㅁ" + i)
+//                     .build();
 
-            boardRepository.save(board);
+//             boardRepository.save(board);
 
-        }
+//         }
 
-        log.info("------------------------------");
-        log.info("End Insert");
+//         log.info("------------------------------");
+//         log.info("End Insert");
 
-    }
+//     }
 
-    @Test
-    @Transactional // lazy loading이라서 걸어줘야 함
-    public void readest() {
-
-
-       IBoardListDTO result = boardRepository.getBoard(5L);
-        log.info(result);
-
-        log.info(result.getBno());
-        log.info(result.getEmail());
-        log.info(result.getCatename());
-        log.info(result.getNickname());
+//     @Test
+//     @Transactional // lazy loading이라서 걸어줘야 함
+//     public void readest() {
 
 
-    }
+//        IBoardListDTO result = boardRepository.getBoard(5L);
+//         log.info(result);
 
-    // @Test
-    // public void readTest() {
+//         log.info(result.getBno());
+//         log.info(result.getEmail());
+//         log.info(result.getCatename());
+//         log.info(result.getNickname());
 
-    //     Optional<Board> result = boardRepository.findById(10L);
 
-    //     Board board = result.orElseThrow();
+//     }
 
-    //     log.info(board);
+//     // @Test
+//     // public void readTest() {
 
-    // }
+//     //     Optional<Board> result = boardRepository.findById(10L);
 
-    @Test
-    public void searchTest(){
+//     //     Board board = result.orElseThrow();
 
-        PageRequestDTO pageRequestDTO = new PageRequestDTO(1, 10, "c", "1", 1);
+//     //     log.info(board);
 
-        PageResponseDTO<BoardListDTO> responseDTO = boardRepository.search(pageRequestDTO);
+//     // }
 
-        log.info(responseDTO);
+//     @Test
+//     public void searchTest(){
 
-    }
+//         PageRequestDTO pageRequestDTO = new PageRequestDTO(1, 10, "c", "1", 1);
 
-}
+//         PageResponseDTO<BoardListDTO> responseDTO = boardRepository.search(pageRequestDTO);
+
+//         log.info(responseDTO);
+
+//     }
+
+// }
